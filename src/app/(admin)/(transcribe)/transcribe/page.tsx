34f439a419c27448adf7e06e943d012a00eff4ea
KZhiganov <KZhiganov@zvezdamedia.ru>
2025-07-09 15:24:21 +0300

    Поправил хлебные крошки а так же нашел где находится таблица и приступил к ее правке

--- src/app/(admin)/(transcribe)/transcribe/page.tsx
+++ src/app/(admin)/(transcribe)/transcribe/page.tsx
@@ -2,6 +2,8 @@ import PageBreadcrumb from "@/components/common/PageBreadCrumb";
 import DropzoneComponent from "@/components/form/form-elements/DropZone";
 import {Metadata} from "next";
 import React from "react";
+import RecentOrders from "@/components/ecommerce/RecentOrders";
+import ListComponent from "@/components/list/ListComponent";
 
 export const metadata: Metadata = {
     title: "AdminTranscrib",
@@ -12,10 +14,13 @@ export const metadata: Metadata = {
 export default function TranscribePage() {
     return (
         <div>
-            <PageBreadcrumb pageTitle="From Elements"/>
+            <PageBreadcrumb pageTitle="Транскрибация"/>
             <div className="grid grid-cols-1 gap-6 xl:grid-cols-2">
                 <div className="col-span-12">
                     <DropzoneComponent/>
+                </div>
+                <div className="col-span-12">
+                    <ListComponent/>
                 </div>
             </div>
         </div>
