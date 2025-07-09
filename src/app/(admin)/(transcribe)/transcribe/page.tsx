70d43986f6e943d70cba3903ad56a81eeee34390
KZhiganov <KZhiganov@zvezdamedia.ru>
2025-07-09 15:15:27 +0300

    Обновил методанные и фавикон. Приступаю к верстке списка

--- src/app/(admin)/(transcribe)/transcribe/page.tsx
+++ src/app/(admin)/(transcribe)/transcribe/page.tsx
@@ -4,9 +4,9 @@ import {Metadata} from "next";
 import React from "react";
 
 export const metadata: Metadata = {
-    title: "Next.js Form Elements | TailAdmin - Next.js Dashboard Template",
+    title: "AdminTranscrib",
     description:
-        "This is Next.js Form Elements page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
+        "!!!",
 };
 
 export default function TranscribePage() {
