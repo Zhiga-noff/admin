fba77f07b93647b768a197eb32969c8f0fb346d5
KZhiganov <KZhiganov@zvezdamedia.ru>
2025-07-09 15:09:46 +0300

    Обновил данные мета и логотип

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -4,8 +4,8 @@ import RecentOrders from "@/components/ecommerce/RecentOrders";
 
 export const metadata: Metadata = {
     title:
-        "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
-    description: "This is Next.js Home for TailAdmin Dashboard Template",
+        "AdminTranscrib",
+    description: "!!!",
 };
 
 export default function Ecommerce() {
