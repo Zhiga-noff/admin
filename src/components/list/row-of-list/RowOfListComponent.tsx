132b80d85550f5d17ab92490fb97fb966d23274c
KZhiganov <KZhiganov@zvezdamedia.ru>
2025-07-11 23:51:48 +0300

    СДелал список аккардион, осталось только добавить стилистические правила

--- src/components/list/row-of-list/RowOfListComponent.tsx
+++ src/components/list/row-of-list/RowOfListComponent.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { TableCell, TableRow } from '@/components/ui/table';
 import Badge from '@/components/ui/badge/Badge';
 import Button from '@/components/ui/button/Button';
@@ -7,6 +7,7 @@ import { ListTypes } from '@/types/list.types';
 import { toFormatDate } from '@/utils/time';
 import { GET_TRANSCRIBATION } from '@/constant/api';
 import { api } from '@/utils/api.services';
+import DropDownList from '@/components/list/row-of-list/drop-down-list/DropDownList';
 
 interface RowOfListProps {
   file: ListTypes;
@@ -15,7 +16,7 @@ interface RowOfListProps {
 
 const RowOfListComponent: FC<RowOfListProps> = ({ file, flag }) => {
   // const { title, inFilePath, outFilePath, done, dateCreate, dateUpdate, id, stateTitle } = file;
-  // const [] = useState(false);
+  const [stateList, setStateList] = useState([]);
 
   const fetchStateList = async () => {
     const result = await api.get(`${GET_TRANSCRIBATION}/${file.id}/stages`, {
@@ -25,68 +26,63 @@ const RowOfListComponent: FC<RowOfListProps> = ({ file, flag }) => {
       },
     });
     console.log(result.data);
+    setStateList(result.data);
   };
 
   const published = toFormatDate(file?.dateCreate);
   const update = toFormatDate(file?.dateUpdate);
-  // console.log(file?.title, file);
-
-  const clickToList = () => {
-    console.log('list');
-  };
-
-  // useEffect(() => {
-  //   ff();
-  // }, []);
 
   return (
-    <TableRow className="">
-      <TableCell className="py-3">
-        <div className="flex items-center gap-3">
-          {/* <div className="h-[50px] w-[50px] overflow-hidden rounded-md flex center"> */}
-          {/*  <Image width={24} height={24} src={'/images/upload.svg'} className="h-[24px] w-[24px]" alt={file.status}/> */}
-          {/* </div> */}
-          <div>
-            <a
-              href={file?.inFilePath ?? ''}
-              className="font-medium underline  text-gray-800 text-theme-sm dark:text-white/90"
-            >
-              {file?.title}
-            </a>
+    <>
+      <TableRow className="">
+        <TableCell className="py-3">
+          <div className="flex items-center gap-3">
+            {/* <div className="h-[50px] w-[50px] overflow-hidden rounded-md flex center"> */}
+            {/*  <Image width={24} height={24} src={'/images/upload.svg'} className="h-[24px] w-[24px]" alt={file.status}/> */}
+            {/* </div> */}
+            <div>
+              <a
+                href={file?.inFilePath ?? ''}
+                className="font-medium underline  text-gray-800 text-theme-sm dark:text-white/90"
+              >
+                {file?.title}
+              </a>
+            </div>
           </div>
-        </div>
-      </TableCell>
-      <TableCell
-        className="py-3 text-gray-500 text-theme-sm dark:text-gray-400 cursor-pointer"
-        onClick={fetchStateList}
-      >
-        🔽
-      </TableCell>
-      {/* {flag && <TableCell className="py-3 text-gray-500 text-theme-sm dark:text-gray-400">{file.category}</TableCell>} */}
-      <TableCell className="py-3 text-gray-500 text-theme-sm dark:text-gray-400">
-        <Badge size="sm" color={file?.done ? 'success' : file?.stateTitle === 'Ошибка' ? 'error' : 'warning'}>
-          {file?.stateTitle}
-        </Badge>
-      </TableCell>
-      <TableCell className="py-3 text-gray-500 text-theme-sm dark:text-gray-400">
-        <span className="text-gray-500 text-theme-xs dark:text-gray-400">Добавлено: {String(published)}</span>
-        <br /> <span className="text-gray-500 text-theme-xs dark:text-gray-400">Обновлено: {String(update)}</span>
-      </TableCell>
-      <TableCell className="py-3 text-gray-500 text-theme-sm dark:text-gray-400">
-        {file?.stateTitle === 'Ошибка' ? (
-          ''
-        ) : (
-          <a href={file?.outFilePath ?? ''}>
-            <Button
-              size="sm"
-              startIcon={<DownloadIcon />}
-              variant={file?.done ? 'primary' : 'outline'}
-              disabled={!file?.done}
-            />
-          </a>
-        )}
-      </TableCell>
-    </TableRow>
+        </TableCell>
+        <TableCell
+          className="py-3 text-gray-500 text-theme-sm dark:text-gray-400 cursor-pointer"
+          onClick={fetchStateList}
+        >
+          🔽
+        </TableCell>
+        {/* {flag && <TableCell className="py-3 text-gray-500 text-theme-sm dark:text-gray-400">{file.category}</TableCell>} */}
+        <TableCell className="py-3 text-gray-500 text-theme-sm dark:text-gray-400">
+          <Badge size="sm" color={file?.done ? 'success' : file?.stateTitle === 'Ошибка' ? 'error' : 'warning'}>
+            {file?.stateTitle}
+          </Badge>
+        </TableCell>
+        <TableCell className="py-3 text-gray-500 text-theme-sm dark:text-gray-400">
+          <span className="text-gray-500 text-theme-xs dark:text-gray-400">Добавлено: {String(published)}</span>
+          <br /> <span className="text-gray-500 text-theme-xs dark:text-gray-400">Обновлено: {String(update)}</span>
+        </TableCell>
+        <TableCell className="py-3 text-gray-500 text-theme-sm dark:text-gray-400">
+          {file?.stateTitle === 'Ошибка' ? (
+            ''
+          ) : (
+            <a href={file?.outFilePath ?? ''}>
+              <Button
+                size="sm"
+                startIcon={<DownloadIcon />}
+                variant={file?.done ? 'primary' : 'outline'}
+                disabled={!file?.done}
+              />
+            </a>
+          )}
+        </TableCell>
+      </TableRow>
+      {stateList.length ? stateList.map((item) => <DropDownList file={item} />) : ''}
+    </>
   );
 };
 
--- src/components/list/row-of-list/drop-down-list/DropDownList.tsx
+++ src/components/list/row-of-list/drop-down-list/DropDownList.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { TableCell, TableRow } from '@/components/ui/table';
+import { ListTypes } from '@/types/list.types';
+import { toFormatDate } from '@/utils/time';
 
-const DropDownList = () => {
-  return <div />;
+interface ListProps {
+  file: ListTypes;
+}
+
+const DropDownList: FC<ListProps> = ({ file }) => {
+  const published = toFormatDate(file?.dateCreate);
+  const update = toFormatDate(file?.dateUpdate);
+
+  return (
+    <TableRow className="">
+      <TableCell className="py-3">
+        <div className="flex items-center gap-3">
+          {/* <div className="h-[50px] w-[50px] overflow-hidden rounded-md flex center"> */}
+          {/*  <Image width={24} height={24} src={'/images/upload.svg'} className="h-[24px] w-[24px]" alt={file.status}/> */}
+          {/* </div> */}
+          <div>
+            <a
+              href={file?.outFilePath ?? ''}
+              className="font-medium underline  text-gray-800 text-theme-sm dark:text-white/90"
+            >
+              {file?.stateTitle}
+            </a>
+          </div>
+        </div>
+      </TableCell>
+      <TableCell className="py-3 text-gray-500 text-theme-sm dark:text-gray-400 cursor-pointer">ㅤ</TableCell>
+      {/* {flag && <TableCell className="py-3 text-gray-500 text-theme-sm dark:text-gray-400">{file.category}</TableCell>} */}
+      <TableCell className="py-3 text-gray-500 text-theme-sm dark:text-gray-400">ㅤ</TableCell>
+      <TableCell className="py-3 text-gray-500 text-theme-sm dark:text-gray-400">
+        <span className="text-gray-500 text-theme-xs dark:text-gray-400">Добавлено: {String(published)}</span>
+        <br /> <span className="text-gray-500 text-theme-xs dark:text-gray-400">Обновлено: {String(update)}</span>
+      </TableCell>
+      <TableCell className="py-3 text-gray-500 text-theme-sm dark:text-gray-400">ㅤ</TableCell>
+    </TableRow>
+  );
 };
 
 export default DropDownList;
